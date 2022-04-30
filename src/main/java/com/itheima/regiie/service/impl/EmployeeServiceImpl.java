72125b68392aea9fb2959f135be8aa3e5baef280
Levi <1521681359@qq.com>
2022-05-01 00:14:56 +0800

    练习Spring MVC

---
+++ src/main/java/com/itheima/regiie/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.itheima.regiie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.regiie.entity.Employee;
+import com.itheima.regiie.mapper.EmployeeMapper;
+import com.itheima.regiie.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper,Employee> implements EmployeeService {
+}
