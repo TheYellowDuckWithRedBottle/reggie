3c37b7db3597e2d8b13a8331096f6456b6a44774
Levi <1521681359@qq.com>
2022-05-01 00:37:08 +0800

    Revert "练习Spring MVC"
    This reverts commit 72125b68392aea9fb2959f135be8aa3e5baef280.

--- src/main/java/com/itheima/regiie/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.regiie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.regiie.entity.Employee;
-
-
-public interface EmployeeService extends IService<Employee> {
-
-}
