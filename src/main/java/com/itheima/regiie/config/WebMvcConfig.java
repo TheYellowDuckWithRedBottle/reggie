2a0b8610795eb954a1e0300e92a1c70ef9722aa4
Levi <1521681359@qq.com>
2022-05-01 21:20:44 +0800

    添加消息转换器配置，将Long转化为字符串

--- src/main/java/com/itheima/regiie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/regiie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.itheima.regiie.config;
 
+import com.itheima.regiie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -14,6 +19,13 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
         registry.addResourceHandler("/employ/**").addResourceLocations("classpath:/controller/");
+    }
+    @Override
+    protected  void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        log.info("扩展消息转换器");
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0,mappingJackson2HttpMessageConverter);
     }
 
 }
