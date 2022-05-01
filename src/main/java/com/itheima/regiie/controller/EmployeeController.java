9a1b1604605d42f6db4e6b043884f6faa0087541
Levi <1521681359@qq.com>
2022-05-01 21:20:29 +0800

    添加消息转换器配置，将Long转化为字符串

--- src/main/java/com/itheima/regiie/controller/EmployeeController.java
+++ src/main/java/com/itheima/regiie/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.itheima.regiie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.regiie.common.R;
 import com.itheima.regiie.entity.Employee;
 import com.itheima.regiie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
@@ -74,6 +73,42 @@ public class EmployeeController  {
         employee.setUpdateUser(empId);
         employeeService.save(employee);
         return R.success("新增员工成功");
+    }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<Employee>> page(int page,int pageSize,String name){
+        log.info("page={},pageSize,name={}",page,pageSize,name);
+
+        //分页构造器
+        Page pageInfo = new Page(page,pageSize);
+
+        // 构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        queryWrapper.orderByDesc(Employee:: getUpdateTime);
+
+        employeeService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    // 修改员工信息
+    @PutMapping
+    public R<String> update(HttpServletRequest  request, @RequestBody Employee employee){
+        log.info(employee.toString());
+
+        Long empId = (Long)request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+
     }
 
 }
