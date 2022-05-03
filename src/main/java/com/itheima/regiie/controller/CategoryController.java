042bda9c97383f6722a5825a2717b298e712f993
Levi <1521681359@qq.com>
2022-05-03 22:32:53 +0800

    添加分類數據的接口

---
+++ src/main/java/com/itheima/regiie/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package com.itheima.regiie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.regiie.common.R;
+import com.itheima.regiie.entity.Category;
+import com.itheima.regiie.service.CategoryService;
+import com.itheima.regiie.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryService cateService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category");
+        cateService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+
+        cateService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(Long id){
+        log.info("删除的菜品");
+        cateService.removeById(id);
+        return R.success("分类信息删除成功");
+    }
+}
