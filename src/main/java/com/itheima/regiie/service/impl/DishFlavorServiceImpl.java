380d1d88301ff85860dc10ce70b099bdd8a0d48e
fanyutan <fanyutan@gtmap.cn>
2022-05-06 13:51:51 +0800

    添加菜品实体和接口

---
+++ src/main/java/com/itheima/regiie/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,18 @@
+package com.itheima.regiie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.regiie.entity.Category;
+import com.itheima.regiie.entity.DishFlavor;
+import com.itheima.regiie.mapper.CategoryMapper;
+import com.itheima.regiie.mapper.DishFlavorMapper;
+import com.itheima.regiie.service.DishFlavorService;
+
+/**
+ * @ClassName
+ * @Description TODO
+ * @Author LeviFan
+ * @Date 2022/5/6 13:47
+ * @Version 1.0
+ **/
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+}
