98e24b22fa839fab97b1bb6f5b2f739d3d22277b
fanyutan <fanyutan@gtmap.cn>
2022-05-06 13:51:15 +0800

    文件上传下载接口

--- src/main/java/com/itheima/regiie/controller/CategoryController.java
+++ src/main/java/com/itheima/regiie/controller/CategoryController.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("category")
@@ -47,5 +49,19 @@ public class CategoryController {
         log.info("修改分类");
         cateService.updateById(_category);
         return null;
+    }
+
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category){
+        // 条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+
+        //添加条件
+        queryWrapper.eq(category.getType()!=null,Category::getType,category.getType());
+        // 添加排序条件
+        queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
+
+        List<Category> list = cateService.list(queryWrapper);
+        return R.success(list);
     }
 }
---
+++ src/test/UploadFileTest.java
@@ -0,0 +1,1 @@
+package com.itheima.regiie.test;
